5a4dbd5fce4a96a56560aabf47181c3c315a14dd
Parth Italiya <133667675+italiyaparth@users.noreply.github.com>
2024-01-27 00:13:48 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,10 +1,7 @@
-// Step 2
-
 const mongoose = require("mongoose");
-const Review = require("./review.js"); // Step 32
+const Review = require("./review.js");
 
 const Schema = mongoose.Schema;
-
 
 const listingSchema = new Schema({
 
@@ -17,18 +14,7 @@ const listingSchema = new Schema({
     type: String,
   },
 
-  // image: {
-  //   type: String, //  URL
-  //   default:
-  //     "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-  //   set: (userImage) =>
-  //     userImage === ""
-  //       ? "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-  //       : userImage,
-  //   // user uploaded image, and uploaded in our different database which stores only images but from there URL link didn't come then default image will come from here
-  // },
-
-  image: { // Step 51
+  image: { 
     url: String,
     filename: String
   },
@@ -45,7 +31,6 @@ const listingSchema = new Schema({
     type: String,
   },
 
-// Step 24
   reviews: [
     {
       type: Schema.Types.ObjectId,
@@ -53,13 +38,11 @@ const listingSchema = new Schema({
     }
   ],
 
-// Step 45
   owner: {
     type: Schema.Types.ObjectId,
     ref: "User"
   },
 
-// Step 52
   geometry: {
     type: {
       type: String,   // Do not do { location: { type: String } }
@@ -72,7 +55,6 @@ const listingSchema = new Schema({
     }
   },
 
-// Step 53
   category: {
     type: String,
     enum: ["mountains", "farms", "cabins", "beach", "castles"],
@@ -82,7 +64,6 @@ const listingSchema = new Schema({
 });
 
 
-// Step 32
 listingSchema.post("findOneAndDelete", async (listing) => {
   if(listing) {
     await Review.deleteMany({ _id: { $in: listing.reviews } });
