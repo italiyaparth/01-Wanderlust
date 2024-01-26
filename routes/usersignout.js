84590e716c4dc49894d0070daf41b4a8acd1ea06
Parth Italiya <133667675+italiyaparth@users.noreply.github.com>
2024-01-27 00:23:49 +0530

    Update usersignout.js

--- routes/usersignout.js
+++ routes/usersignout.js
@@ -1,27 +1,10 @@
-// Step 41
-
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../controllers/users.js"); // Step 48
+const userController = require("../controllers/users.js");
 
-/*
-router.get("/", (req, res, next) => {
 
-    req.logout((err) => {
-
-        if (err) {
-            return next(err);
-        }
-
-        req.flash("success", "You are Logged Out");
-        res.redirect("/listings");
-    });
-
-});
-*/
-
-// Step 48
 router.get("/", userController.signout);
+
 
 module.exports = router;
