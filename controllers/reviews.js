ee4f21d15322708a98f2d8f05f46b19ca2593732
Parth Italiya <133667675+italiyaparth@users.noreply.github.com>
2024-01-27 00:07:15 +0530

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,19 +1,17 @@
-// Step 48
-
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 
 module.exports.createReview = async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;    // Step 47
+    newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
 
     await listing.save();
     await newReview.save();
 
-    req.flash("success", "New Review Created!");   // Step 35
+    req.flash("success", "New Review Created!");
 
     res.redirect(`/listings/${listing._id}`);
 };
@@ -24,7 +22,7 @@ module.exports.destroyReview = async (req, res, next) => {
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
 
-    req.flash("success", "Review Deleted!");   // Step 35
+    req.flash("success", "Review Deleted!");
 
     res.redirect(`/listings/${id}`);
 };
